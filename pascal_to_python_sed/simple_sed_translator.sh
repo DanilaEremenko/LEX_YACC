1f1a4010f8cff385f894441e2fadcec86c687737
DanilaEremenko <danilaeremenko@mail.ru>
2019-11-19 18:15:54 +0300

    pascal_to_python_sed: first one-test-passing version added

--- pascal_to_python_sed/simple_sed_translator.sh
+++ pascal_to_python_sed/simple_sed_translator.sh
@@ -6,6 +6,10 @@ die(){
 	exit 1
 }
 
+pretty_print(){
+	printf "\n--------------------\n$*\n-----------------------\n"
+}
+
 INPUT_FILE=""
 OUTPUT_FILE=""
 TEST_LIST=""
@@ -34,17 +38,106 @@ else
 fi
 
 if [[ $TEST_LIST ]];then
-  for test_name in $TEST_LIST;do
-		rm -f *-res.py
+	#------------------------ MAKE TESTS -----------------------------------------
+	tpassed_num=0
+	tpassed_list=""
 
+	tfailed_num=0
+	tfailed_list=""
+
+	tfull_num=0
+
+	rm -f $TEST_DIR/*-res.py
+
+	for test_name in $TEST_LIST;do
+
+		#------------------------ PREPARE FOR TRANSLATION --------------------------
 		test_file="$TEST_DIR/$test_name"
     printf "$test_file\n"
+		res_file="$TEST_DIR/$test_name-res.py"
+		touch $res_file
+		tabs=""
+		new_tabs=""
+		old_tabs=""
+		prev_func=""
+		prev_if=""
 
+		#------------------------ TRANSLATE FILE LOOP ------------------------------
 		while read line ;do
-			res_file="$TEST_DIR/$test_name-res.py"
-			touch $res_file
-			echo -e $line  >> $res_file
+			target_line="$line"
+			#-------------------- IF PROCESSING --------------------------------------
+			if [[ $(echo $line | grep 'if') ]];then
+				target_line=$(echo $target_line | sed 's/ then/:/')
+				pretty_print "IF LINE:'$line' -> '$target_line'"
 
+				if [[ $(echo $target_line | grep ';') ]];then prev_if=""
+				else prev_if="true";new_tabs="$tabs\t";fi
+
+			#-------------------- BEGIN PROCESSING -----------------------------------
+			elif [[ $(echo $line | grep 'begin') ]];then
+				target_line=$(echo $target_line | sed 's/begin//')
+				pretty_print "BEGIN LINE:'$line' -> '$target_line'"
+
+				if [[ $prev_func || $prev_if ]];then prev_func="";prev_if="";
+				else new_tabs="$tabs\t";fi
+
+			#-------------------- FUNCTION PROCESSING --------------------------------
+			elif [[ $(echo $line | grep 'function') ]];then
+				target_line="$(echo $target_line | sed 	-e 's/function/def/'\
+																								-e 's/\ : .*)/)/'\
+																								-e 's/\: .*;//'\
+																								):"
+				pretty_print "FUNCTION LINE:'$line' -> '$target_line'"
+
+				new_tabs="$tabs\t"
+				prev_func="true"
+			#-------------------- VAR PROCESSING -------------------------------------
+			elif [[ $(echo $line | grep 'var ') ]];then
+				target_line="$(echo $target_line | sed 	-e 's/var //'\
+																								-e 's/,/ =/'\
+																								-e 's/\: .*;/= 0.0/'\
+																								)"
+				pretty_print "VAR LINE:'$line' -> '$target_line'"
+
+			#-------------------- END PROCESSING -------------------------------------
+			elif [[ $(echo $line | grep 'end') ]];then
+				target_line=$(echo $target_line | sed 's/end;//')
+				pretty_print "END LINE:'$line' -> '$target_line'"
+
+				new_tabs=$(echo $tabs | cut -c3-)
+
+			#-------------------- CHECK  PREV IF -------------------------------------
+			elif [[ $prev_if ]];then
+				prev_if=""
+				new_tabs=$(echo $tabs | cut -c3-)
+			else
+				pretty_print "LINE:'$line' -> '$target_line'"
+			fi
+
+			target_line=$(echo "$target_line" | sed -e 's/:=/=/' -e 's/;//')
+
+			echo -e "$tabs$target_line"  >> $res_file
+
+			old_tabs=$tabs
+			tabs=$new_tabs
 		done < $test_file.pas
-  done
+
+	#-------------------------- CHECK TEST ---------------------------------------
+	diff_test=$(diff "$TEST_DIR/$test_name-exp.py" "$TEST_DIR/$test_name-res.py")
+
+	if [[ $diff_test ]];then
+		((tfailed_num++))
+		tfailed_list="$tfailed_list $test_name,"
+	else
+		((tpassed_num++))
+		tpassed_list="$tpassed_list $test_name,"
+	fi
+	((tfull_num++))
+	done
+
+	printf "\n\n#################### TEST RESULTS ############################\n\n"
+	printf "PASSED_NUM = $tpassed_num\n"
+	printf "PASSED_LIST = $tpassed_list\n\n"
+	printf "FAILED_NUM = $tfailed_num\n"
+	printf "FAILED_LIST = $tfailed_list\n\n"
 fi
---
+++ pascal_to_python_sed/tests/SimpleFunction-exp.py
@@ -0,0 +1,10 @@
+def AP(arg1, arg2) :
+	a = al = 0.0
+	
+	if (cLAI>1e-8):
+		sleep(0)
+	a = -RConst1*cLAI/shour
+	if a<-9.0: a=-9.0
+	al = RConst2 * (exp(a*RConst3)-exp(a))
+	result = (dir_diff*exp(a)+exp(-cLAI))/(dir_diff+1) + al
+	
